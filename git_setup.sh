f1da28531314d0c5e5bd312f726b6eb3ea9506ff
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2019-04-09 12:46:38 +0900

    create git_setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,5 @@
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --list
