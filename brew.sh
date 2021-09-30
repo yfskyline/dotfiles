e95b39447ecd258fdf975ba8aa4f69ecd3f95002
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2021-09-30 14:41:31 +0900

    Remove: verbose codes

--- brew.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-defaults write com.google.drivefs.settings BandwidthRxKBPS -int 1000
-defaults write com.google.drivefs.settings BandwidthTxKBPS -int 1000
