99943c73c86d7e12453a87deb3aafff0da1bc91d
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2021-10-03 01:32:44 +0900

    Update

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -2,5 +2,6 @@ sudo apt install zsh
 cd $HOME/dotfiles
 sh dotfilesLink.sh
 sudo chsh $USER -s $(which zsh)
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade && sudo apt autoremove
 sudo update-alternatives --set editor /usr/bin/vim.basic
+source $HOME/dotfiles/.zshrc
