3660cdaaaa84f5de56aea7e909e195faca1999df
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2021-10-21 15:16:44 +0900

    Improve: add -y option to upgrade

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -2,6 +2,6 @@ sudo apt install zsh
 cd $HOME/dotfiles
 sh dotfilesLink.sh
 sudo chsh $USER -s $(which zsh)
-sudo apt update && sudo apt upgrade && sudo apt autoremove
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove
 sudo update-alternatives --set editor /usr/bin/vim.basic
 source $HOME/dotfiles/.zshrc
