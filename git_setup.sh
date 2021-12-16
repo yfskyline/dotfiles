4bfdd4c26bc67b1a408aa3a6130a897883e6dfe6
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2021-12-17 02:33:56 +0900

    Add: username / email

--- git_setup.sh
+++ git_setup.sh
@@ -1,4 +1,6 @@
-git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global user.name "Yuta Fukagawa"
+git config --global user.email 25516089+yfskyline@users.noreply.github.com
+git config --global core.editor vim
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
