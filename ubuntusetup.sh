3ad4696f8f80bacdce941ce2747f77be0936300c
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-02-25 20:25:47 +0900

    Add: apt-fast

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -1,9 +1,12 @@
-sudo apt install -y zsh curl ssh git vim tmux
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt install apt-fast -y
+sudo apt-fast install -y zsh curl ssh git vim tmux
 cd $HOME/dotfiles
 sh dotfilesLink.sh
 sudo chsh $USER -s $(which zsh)
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+sudo apt-fast update && sudo apt-fast upgrade -y && sudo apt-fast autoremove && sudo apt-fast cleanup
 sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo sh ./ssh-keys.sh
+sudo sh NeoBundle_install.sh
 sudo sh cron.sh
 source $HOME/dotfiles/.zshrc
