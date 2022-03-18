1e8117ef277c769fa7c6539ea86dc9a17c6a7b69
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-19 01:29:14 +0900

    Add: Keyrepeat

--- mac/defaults/setup_keyboard.sh
+++ mac/defaults/setup_keyboard.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# KeyBoard
+defaults write -g KeyRepeat -int 1
+# defaults write -g KeyRepeat -int 2 # システム上限
+defaults write -g InitialKeyRepeat -int 10
+# defaults write -g InitialKeyRepeat -int 15 # システム上限
+
+
+# User Dictionary
+
+# Shortcuts
+
+# Input Source
+
+# Voice Input
