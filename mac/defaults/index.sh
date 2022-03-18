504edd5d547862c4b265621c92023cfe39752ef0
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-19 01:15:19 +0900

    Create: setup_keyboard.sh

--- mac/defaults/index.sh
+++ mac/defaults/index.sh
@@ -2,3 +2,4 @@
 sh $HOME/dotfiles/mac/defaults/setup_dock.sh
 sh $HOME/dotfiles/mac/defaults/setup_finder.sh
 sh $HOME/dotfiles/mac/defaults/setup_trackpad.sh
+sh $HOME/dotfiles/mac/defaults/setup_keyboard.sh
