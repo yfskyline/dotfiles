76cea7d8d5ebf58558c8a48886598b0d6f188854
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-18 21:40:26 +0900

    Move: dock related direction to setup_dock.sh

--- mac/defaults/setup_dock.sh
+++ mac/defaults/setup_dock.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+# Dock: 自動的に隠す
+defaults write com.apple.dock autohide -bool true
+
+# MissionControl
+# 最近の使用率に応じて並べ替える
+defaults write com.apple.dock mru-spaces -bool true
+
+# アプリケーションごとにウィンドウをまとめる
+defaults write com.apple.dock expose-group-apps -bool true
+
+# HotCorner
+defaults write com.apple.dock wvous-tl-corner -int 13 # 画面をロック
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+# Dock icon size
+defaults write com.apple.dock tilesize -int 39
+
+# Automatically hide or show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Wipe all app icons from the Dock exclude Finder and Trash
+defaults write com.apple.dock persistent-apps -array
+
+# 設定を反映
+killall Dock
--- mac/setup_mac.sh
+++ mac/setup_mac.sh
@@ -15,42 +15,14 @@ brew bundle --file $HOME/dotfiles/mac/Brewfile
 
 # システム環境設定(defaults command)
 # -----------------------------------
-
-# Dock: 自動的に隠す
-defaults write com.apple.dock autohide -bool true
-
 # マウスカーソル速度設定
 defaults write "Apple Global Domain" com.apple.mouse.scaling 3.0
-
-# MissionControl
-# 最近の使用率に応じて並べ替える
-defaults write com.apple.dock mru-spaces -bool true
-
-# アプリケーションごとにウィンドウをまとめる
-defaults write com.apple.dock expose-group-apps -bool true
-
-# HotCorner
-defaults write com.apple.dock wvous-tl-corner -int 13 # 画面をロック
-defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # Finder:隠しファイル/フォルダを表示
 defaults write com.apple.finder AppleShowAllFiles true
 
 # Finder:拡張子を表示
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Dock icon size
-defaults write com.apple.dock tilesize -int 39
-
-# Automatically hide or show the Dock
-defaults write com.apple.dock autohide -bool true
-
-# Wipe all app icons from the Dock exclude Finder and Trash
-defaults write com.apple.dock persistent-apps -array
-
-# 設定を反映
-killall Dock
-
 
 # Finder
 # Set `${HOME}` as the default location for new Finder windows
@@ -62,8 +34,6 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 #Interface Theme: Dark
 #Editor Theme: night
 # ------------------------------------
-
-
 
 # Visual Studio Code
 # ln -sf ~/dotfiles/vscode/settings.json ~/settings.json
