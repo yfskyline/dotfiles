e1b1f95bbf95bfd18440d9f0fc43cd03760c2f3e
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-21 17:36:09 +0900

    Add: other options

--- mac/defaults/setup_ical.sh
+++ mac/defaults/setup_ical.sh
@@ -2,3 +2,8 @@
 # iCal(Calendar)
 
 defaults write com.apple.iCal "Show Week Numbers" -bool true
+
+# default duration to 15m
+# defaults write com.apple.iCal "Default duration in minutes for new event" 15
+# 24 hour view
+# defaults write com.apple.iCal "number of hours displayed" 24
