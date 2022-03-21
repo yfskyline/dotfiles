7fe0148b132d3ed6782c82fcd77a68089a434b29
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-21 15:05:42 +0900

    Add: spotlight settings

--- mac/defaults/index.sh
+++ mac/defaults/index.sh
@@ -3,3 +3,4 @@ sh $HOME/dotfiles/mac/defaults/setup_dock.sh
 sh $HOME/dotfiles/mac/defaults/setup_finder.sh
 sh $HOME/dotfiles/mac/defaults/setup_trackpad.sh
 sh $HOME/dotfiles/mac/defaults/setup_keyboard.sh
+sh $HOME/dotfiles/mac/defaults/setup_spotlight.sh
