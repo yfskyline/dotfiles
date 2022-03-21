06b9ae37321367f2780736dc08c6e37f9b09dab6
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-21 15:28:15 +0900

    Create: ical settings

--- mac/defaults/index.sh
+++ mac/defaults/index.sh
@@ -4,3 +4,4 @@ sh $HOME/dotfiles/mac/defaults/setup_finder.sh
 sh $HOME/dotfiles/mac/defaults/setup_trackpad.sh
 sh $HOME/dotfiles/mac/defaults/setup_keyboard.sh
 sh $HOME/dotfiles/mac/defaults/setup_spotlight.sh
+sh $HOME/dotfiles/mac/defaults/setup_ical.sh
---
+++ mac/defaults/setup_ical.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# iCal(Calendar)
+
+defaults write com.apple.iCal "Show Week Numbers" -bool true
