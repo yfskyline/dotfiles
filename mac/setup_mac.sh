93629125cb4e0c42e186d73e7f222dba44845725
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-22 16:46:57 +0900

    Remove: duplicate settings

--- mac/setup_mac.sh
+++ mac/setup_mac.sh
@@ -19,24 +19,6 @@ fi
 
 brew bundle --file $HOME/dotfiles/mac/Brewfile
 
-# システム環境設定(defaults command)
-# -----------------------------------
-# マウスカーソル速度設定
-defaults write "Apple Global Domain" com.apple.mouse.scaling 3.0
-
-# Finder:隠しファイル/フォルダを表示
-defaults write com.apple.finder AppleShowAllFiles true
-
-# Finder:拡張子を表示
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Finder
-# Set `${HOME}` as the default location for new Finder windows
-defaults write com.apple.finder NewWindowTarget -string "PfDe"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
-# ------------------------------------
-
-
 #Interface Theme: Dark
 #Editor Theme: night
 # ------------------------------------
