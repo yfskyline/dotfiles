91268af7bd565356d97a980a808a780fa520ad75
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-22 16:41:23 +0900

    Add: some options

--- mac/defaults/setup_finder.sh
+++ mac/defaults/setup_finder.sh
@@ -3,4 +3,26 @@
 # show hidden files in finder
 # defaults write com.apple.finder AppleShowAllFiles YES
 
+# Automatically open a new Finder window whe a volume is mounted
+# defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+# defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+# defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Set `${HOME}` as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+# Show Status bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Show Path bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show Tab bar in Finder
+defaults write com.apple.finder ShowTabView -bool true
+
+# Show the ~/Library directory
+chflags nohidden ~/Library
+
+
 killall Finder # enable Finder setting changes
