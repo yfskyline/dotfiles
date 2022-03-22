74105cb498345293e89f2e041a36c9f3a468d32b
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-22 16:35:18 +0900

    Add: HotCorner options

--- mac/defaults/setup_dock.sh
+++ mac/defaults/setup_dock.sh
@@ -83,8 +83,30 @@ defaults write com.apple.systemuiserver "menuExtras" -array "/System/Library/Cor
 # defaults write com.apple.dock expose-group-apps -bool true
 
 # HotCorner
+## Possible values:
+## 0: no-op
+## 2: Mission Control
+## 3: Show application windows
+## 4: Desktop
+## 5: Start Screen Saver
+## 6: Disable Screen Saver
+## 7: Dashboard
+## 10: Put display to sleep
+## 11: Launchpad
+## 12: Notification Center
+# Top left
 # defaults write com.apple.dock wvous-tl-corner -int 13 # 画面をロック
 # defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right
+# defaults write com.apple.dock wvous-tr-corner -int 13 # 画面をロック
+# defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left
+# defaults write com.apple.dock wvous-bl-corner -int 13 # 画面をロック
+# defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right
+# defaults write com.apple.dock wvous-br-corner -int 13 # 画面をロック
+# defaults write com.apple.dock wvous-br-modifier -int 0
+
 
 # Wipe all app icons from the Dock exclude Finder and Trash
 defaults write com.apple.dock persistent-apps -array
