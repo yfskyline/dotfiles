4c026a124053b89cdc9ec829fbe5cfe9d1afaef9
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-24 00:03:48 +0900

    Add: mouse scroll speed

--- mac/defaults/setup_mouse.sh
+++ mac/defaults/setup_mouse.sh
@@ -3,8 +3,14 @@
 
 # タップしたときに、クリックとする
 defaults write -g com.apple.mouse.tapBehavior -int 1
+
 # Mouse Speed
 defaults write -g com.apple.mouse.scaling 3
+
+# Mouse Scroll Speed
+defaults write -g com.apple.scrollwheel.scaling 5
+
+
 # 3本指でmission control & expose
 # defaults write com.apple.dock showMissionControlGestureEnabled -bool true
 defaults write com.apple.dock showAppExposeGestureEnabled -bool true
