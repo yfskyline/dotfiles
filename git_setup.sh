b5c2874fb71d2eb3c6f709822f5d92de3db507c9
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-23 23:44:47 +0900

    Add: setup git ignore global

--- git_setup.sh
+++ git_setup.sh
@@ -5,3 +5,5 @@ git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
 git config --list
+
+mkdir -p ~/.config/git && cp .gitignore_global ~/.config/git/ignore
