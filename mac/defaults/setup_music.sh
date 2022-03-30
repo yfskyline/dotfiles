f519bd958459a83f6be12dd84278f8081f0a598c
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-30 15:31:41 +0900

    Add: disable DolbyAtmos

--- mac/defaults/setup_music.sh
+++ mac/defaults/setup_music.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 defaults write com.apple.Music checkForAvailableDownloads -bool true
 defaults write com.apple.Music losslessEnabled -bool true
+defaults write com.apple.Music preferredDolbyAtmosPlaySetting -int 30
+defaults write com.apple.Music multichannelAudioStrategy -bool true
