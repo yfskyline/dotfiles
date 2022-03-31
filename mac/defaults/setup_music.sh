1d460fb97aee2bb564dbf1fd6e2f9a9e5088da09
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2022-03-31 23:07:28 +0900

    Add: disable optimization of volume

--- mac/defaults/setup_music.sh
+++ mac/defaults/setup_music.sh
@@ -3,3 +3,4 @@ defaults write com.apple.Music checkForAvailableDownloads -bool true
 defaults write com.apple.Music losslessEnabled -bool true
 defaults write com.apple.Music preferredDolbyAtmosPlaySetting -int 30
 defaults write com.apple.Music multichannelAudioStrategy -bool true
+defaults write com.apple.Music optimizeSongVolume -bool false
