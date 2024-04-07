ff58c53046156fc4bce3196462e547862b3cf4ef
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2024-04-08 02:47:41 +0900

    UPDATE: remove autocrlf

--- git_setup.sh
+++ git_setup.sh
@@ -1,7 +1,6 @@
 git config --global user.name "Yuta Fukagawa"
 git config --global user.email 25516089+yfskyline@users.noreply.github.com
 git config --global core.editor vim
-git config --global core.autocrlf true
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
