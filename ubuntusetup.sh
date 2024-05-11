5288584524d2833ed7ba795566c9f45f4f7fc691
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2024-05-12 02:24:55 +0900

    ADD: timezone configuration

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -1,11 +1,13 @@
+sudo timedatectl set-timezone Asia/Tokyo
 sudo add-apt-repository ppa:apt-fast/stable
 sudo apt install apt-fast -y
 sudo apt-fast install -y zsh curl ssh git vim tmux
+sudo apt-fast update && sudo apt-fast upgrade -y && sudo apt-fast autoremove && sudo apt-fast cleanup
 cd $HOME/dotfiles
 sh dotfilesLink.sh
 sudo chsh $USER -s $(which zsh)
-sudo apt-fast update && sudo apt-fast upgrade -y && sudo apt-fast autoremove && sudo apt-fast cleanup
 sudo update-alternatives --set editor /usr/bin/vim.basic
+sh ./ssh-keys.sh
 sudo sh ./ssh-keys.sh
 sudo sh NeoBundle_install.sh
 sudo sh cron.sh
