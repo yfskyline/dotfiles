311ac41b7c2e8bc7a0ce3983be0b60ae2b22fdb6
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-01-24 02:15:23 +0900

    IMPROVE: fix for using by root-user

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-mkdir -p $HOME/.ssh
-curl https://github.com/yfskyline.keys >> $HOME/.ssh/authorized_keys
-chmod 600 $HOME/.ssh/authorized_keys
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run this script with sudo"
+	exit 1
+fi
+
+# Add ssh keys to the user
+mkdir -p /home/$SUDO_USER/.ssh
+curl https://github.com/yfskyline.keys >> /home/$SUDO_USER/.ssh/authorized_keys
+chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.ssh/authorized_keys
+chmod 600 /home/$SUDO_USER/.ssh/authorized_keys
