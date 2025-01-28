99acb63895b2b78f0d9c6adf16066bda7f531329
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-01-29 02:52:36 +0900

    FIX: lint, SC2086,SC3028

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
 	echo "Please run this script with sudo"
@@ -6,7 +6,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Add ssh keys to the user
-mkdir -p /home/$SUDO_USER/.ssh
-curl https://github.com/yfskyline.keys >> /home/$SUDO_USER/.ssh/authorized_keys
-chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.ssh/authorized_keys
-chmod 600 /home/$SUDO_USER/.ssh/authorized_keys
+mkdir -p /home/"$SUDO_USER"/.ssh
+curl https://github.com/yfskyline.keys >> /home/"$SUDO_USER"/.ssh/authorized_keys
+chown "$SUDO_USER":"$SUDO_USER" /home/"$SUDO_USER"/.ssh/authorized_keys
+chmod 600 /home/"$SUDO_USER"/.ssh/authorized_keys
