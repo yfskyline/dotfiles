053972b99b6f7ece8049d21b3283f4aa04e2d9dc
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-01-29 03:24:24 +0900

    FIX: lint

--- ubuntu/install_nvm.sh
+++ ubuntu/install_nvm.sh
@@ -2,12 +2,13 @@
 # Install nvm
 
 # set $OS
-if [ "$(uname)" = 'Darwin' ]; then
+OS_NAME=$(uname -s)
+if [ "$OS_NAME" = 'Darwin' ]; then
 	OS='Mac'
-elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
-	OS='Linux'
-elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
-	OS='Cygwin'
+elif [ "${OS_NAME:0:5}" = 'Linux' ]; then
+    OS='Linux'
+elif [ "${OS_NAME:0:10}" = 'MINGW32_NT' ]; then
+    OS='Cygwin'
 else
 	echo -e "${LOG}Your platform ($(uname -a)) is not supported."
 	exit 1
@@ -15,7 +16,7 @@ fi
 
 if [ $OS = 'Mac' ]; then
 	brew install nvm
-	mkdir $HOME/.nvm
+	mkdir "$HOME"/.nvm
 elif [ $OS = 'Linux' ]; then
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 fi
