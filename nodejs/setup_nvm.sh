edc1c09b4f04e0305feb5329e3f90a678fadcde5
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-02-01 18:49:29 +0900

    FIX: error handling

--- nodejs/setup_nvm.sh
+++ nodejs/setup_nvm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Install nvm
 
+# define color / log headers
+RED="\e[31;1m"
+GREEN="\e[32;1m"
+YELLOW="\e[33;1m"
+BLUE="\e[34;1m"
+CYAN="\033[36;1m"
+RESET="\e[0m"
+SUCCESS="${GREEN}[SUCCESS]${RESET}"
+FAILED="${RED}[FAILED ]${RESET}"
+LOG="${CYAN}[LOG    ]${RESET}"
+
 # set $OS
 OS_NAME=$(uname -s)
 if [ "$OS_NAME" = 'Darwin' ]; then
@@ -10,13 +21,13 @@ elif [ "${OS_NAME:0:5}" = 'Linux' ]; then
 elif [ "${OS_NAME:0:10}" = 'MINGW32_NT' ]; then
 	OS='Cygwin'
 else
-	echo -e "${LOG}Your platform ($(uname -a)) is not supported."
+	echo -e "${FAILED}Your platform ($(uname -a)) is not supported."
 	exit 1
 fi
 
 if [ $OS = 'Mac' ]; then
 	brew install nvm
-	mkdir "$HOME"/.nvm
+	mkdir -p "$HOME"/.nvm
 elif [ $OS = 'Linux' ]; then
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 fi
