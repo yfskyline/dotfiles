6bc9f1a5f7cf777382fe04617db9632254820e9b
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-02-03 02:00:29 +0900

    IMPROVE: suppress apt output

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -16,8 +16,8 @@ if [ "$(id -u)" -ne 0 ]; then
 	exit 1
 fi
 
-apt update
-if apt install ca-certificates curl; then
+apt update -qq
+if apt install -qq ca-certificates curl; then
 	echo -e "$SUCCESS Installed required packages(ca-certificates and curl)"
 else
 	echo -e "$FAILED Failed to install ca-certificates and curl"
@@ -30,11 +30,12 @@ echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-if apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; then
-	echo -e "$SUCCESS Docker installed successfully"
+apt update -qq
+if apt install -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null; then
+	echo -e "$SUCCESS packages required by Docker has installed successfully"
 else
 	echo -e "$FAILED Docker installation failed"
+	echo -e "$SUCCESS packages required by Docker has installed successfully"
 	exit 1
 fi
 
