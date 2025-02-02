54e704aea78e67e3b7857b2f4beb713d6ecc3ebb
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-02-03 01:46:46 +0900

    FIX: indent

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -12,8 +12,8 @@ FAILED="${RED}[FAILED ]${RESET}"
 LOG="${CYAN}[LOG    ]${RESET}"
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo -e "$FAILED Please run as root"
-  exit 1
+	echo -e "$FAILED Please run as root"
+	exit 1
 fi
 
 apt update
@@ -22,9 +22,9 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker run hello-world
@@ -33,11 +33,16 @@ systemctl start docker
 systemctl enable docker
 echo -e "$LOG docker status: $(systemctl is-active docker)"
 if systemctl is-active docker; then
-  echo -e "$SUCCESS Docker installed successfully"
+	echo -e "$SUCCESS Docker installed successfully"
 else
-  echo -e "$FAILED Docker installation failed"
-  exit 1
+	echo -e "$FAILED Docker installation failed"
+	exit 1
 fi
-echo "docker version"
-docker version
-usermod -aG docker "$SUDO_USER"
+echo "$LOG docker version"
+docker version | grep -v 'commit|Commit|Experimental'
+if usermod -aG docker "$SUDO_USER"; then
+	echo -e "$SUCCESS Added $SUDO_USER to docker group"
+else
+	echo -e "$FAILED Failed to add $SUDO_USER to docker group"
+	exit 1
+fi
