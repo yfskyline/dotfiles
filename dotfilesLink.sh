1407cc66858b3566c3abd5d5120b53d9cdedde5f
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-02-09 21:18:09 +0900

    ADD: karabiner / vscode settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+# set $OS
+if [ "$(uname)" = 'Darwin' ]; then
+	OS='Mac'
+elif [ "$(uname -s | cut -c1-5)" = 'Linux' ]; then
+	OS='Linux'
+elif [ "$(uname -s | cut -c1-10)" = 'MINGW32_NT' ]; then
+	OS='Cygwin'
+else
+	echo -e "${FAILED}Your platform ($(uname -a)) is not supported."
+	exit 1
+fi
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
@@ -7,3 +20,26 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.digrc ~/.digrc
 ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+
+# macos
+if [ $OS = 'Mac' ]; then
+	# Karabiner-Elements config
+	mkdir -p ~/.config/karabiner
+	ln -sf ~/dotfiles/mac/karabiner/assets ~/.config/karabiner/assets
+	ln -sf ~/dotfiles/mac/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+
+	# Visual Studio Code
+	ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+	ln -sf ~/dotfiles/vscode/argv.json ~/.vscode/argv.json
+	# Interface Theme: Dark
+	# Editor Theme: night
+elif [ $OS = 'Linux' ]; then
+	# Visual Studio Code
+	ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+	ln -sf ~/dotfiles/vscode/argv.json ~/.vscode/argv.json
+	# Interface Theme: Dark
+	# Editor Theme: night
+else
+	echo -e "${FAILED}Your platform ($(uname -a)) is not supported."
+	exit 1
+fi
