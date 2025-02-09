ec48bc0e79d15c258342de6b4ed157f83bc81c0a
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-02-10 03:46:07 +0900

    FIX: filepath for macOS

--- ssh/setup_authorized_keys.sh
+++ ssh/setup_authorized_keys.sh
@@ -37,9 +37,9 @@ if [ "$OS" = 'Linux' ]; then
 	echo "*/10 * * * * ssh-import-id gh:yfskyline" >> /etc/crontab
 elif [ "$OS" = 'Darwin' ]; then
 	# Add ssh keys to the user
-	mkdir -p /home/"$SUDO_USER"/.ssh
-	curl https://github.com/yfskyline.keys >> /home/"$SUDO_USER"/.ssh/authorized_keys
-	chown "$SUDO_USER":"$SUDO_USER" /home/"$SUDO_USER"/.ssh/authorized_keys
-	chmod 600 /home/"$SUDO_USER"/.ssh/authorized_keys
+	mkdir -p /Users/"$SUDO_USER"/.ssh
+	curl https://github.com/yfskyline.keys >> /Users/"$SUDO_USER"/.ssh/authorized_keys
+	chown "$SUDO_USER":"$SUDO_USER" /Users/"$SUDO_USER"/.ssh/authorized_keys
+	chmod 600 /Users/"$SUDO_USER"/.ssh/authorized_keys
 	uniq -u authorized_keys > tmp && mv -f tmp authorized_keys
 fi
