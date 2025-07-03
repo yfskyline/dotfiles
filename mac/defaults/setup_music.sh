863582e2f0b6fdf326ba1de95cadff07a41abe4b
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-07-03 18:17:36 +0900

    ADD: setup SoundEnhancer

--- mac/defaults/setup_music.sh
+++ mac/defaults/setup_music.sh
@@ -4,3 +4,7 @@ defaults write com.apple.Music losslessEnabled -bool true
 defaults write com.apple.Music preferredDolbyAtmosPlaySetting -int 30
 defaults write com.apple.Music multichannelAudioStrategy -bool true
 defaults write com.apple.Music optimizeSongVolume -bool false
+
+# SoundEnhancer
+defaults write com.apple.Music soundEnhancerAmount 200
+defaults write com.apple.Music soundEnhancerEnabled 1
