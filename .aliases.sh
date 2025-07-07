988ab0f64b244546b1f9c3b3c6c2866e3cf7d0b1
Yuta Fukagawa <25516089+yfskyline@users.noreply.github.com>
2025-07-08 02:01:44 +0900

    ADD: add alias apt for apt-fast

--- .aliases.sh
+++ .aliases.sh
@@ -37,7 +37,7 @@ alias destroy='sudo containerlab destroy'
 alias ffmpeg='ffmpeg -hide_banner'
 alias color='for fore in `seq 30 37`; do printf "\e[${fore}m \\\e[${fore}m \e[m\n"; for mode in 1 4 5; do printf "\e[${fore};${mode}m \\\e[${fore};${mode}m \e[m"; for back in `seq 40 47`; do printf "\e[${fore};${back};${mode}m \\\e[${fore};${back};${mode}m \e[m"; done; echo; done; echo; done; printf " \\\e[m\n"'
 
-if [[ -n "$ZSH_VERSION" ]]; then
+if [ -n "$ZSH_VERSION" ]; then
 	# Global Alias
 	alias -g L='| less'
 	alias -g G='| grep'
@@ -55,6 +55,7 @@ if [ "$OS" = 'Linux' ]; then
 	alias pbpaste='xsel --clipboard --output'
 	alias ls='ls -F --color=auto'
 	alias fd='fdfind'
+	alias apt='apt-fast'
 elif [ "$OS" = 'Mac' ]; then
 	alias defaults_before='defaults read > before.txt && defaults -currentHost read > beforeCurrent.txt'
 	alias defaults_after='defaults read > after.txt && defaults -currentHost read > afterCurrent.txt && diff --color before.txt after.txt && diff --color beforeCurrent.txt afterCurrent.txt'
